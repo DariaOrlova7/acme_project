7ce37d220634b6e242afcc28d62979d49997426f
Дарья Орлова <orlovadara70@gmail.com>
2026-04-14 17:56:53 +0500

    6 спринт 2 тема 1 урок

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -4,6 +4,8 @@ from .forms import BirthdayForm
 # Импортируем из utils.py функцию для подсчёта дней.
 from .utils import calculate_birthday_countdown
 from .models import Birthday
+# Импортируем класс пагинатора.
+from django.core.paginator import Paginator
 
 
 def birthday(request, pk=None):
@@ -29,8 +31,19 @@ def birthday(request, pk=None):
 
 
 def birthday_list(request):
-    birthdays = Birthday.objects.all()
-    context = {'birthdays': birthdays}
+    # Получаем список всех объектов с сортировкой по id.
+    birthdays = Birthday.objects.order_by('id')
+    # Создаём объект пагинатора с количеством 10 записей на страницу.
+    paginator = Paginator(birthdays, 10)
+    # Получаем из запроса значение параметра page.
+    page_number = request.GET.get('page')
+    # Получаем запрошенную страницу пагинатора. 
+    # Если параметра page нет в запросе или его значение не приводится к числу,
+    # вернётся первая страница.
+    page_obj = paginator.get_page(page_number)
+    # Вместо полного списка объектов передаём в контекст 
+    # объект страницы пагинатора
+    context = {'page_obj': page_obj}
     return render(request, 'birthday/birthday_list.html', context)
 
 
@@ -42,17 +55,3 @@ def delete_birthday(request, pk):
         instance.delete()
         return redirect('birthday:list')
     return render(request, 'birthday/birthday.html', context)
-# from django.shortcuts import render
-# from .forms import BirthdayForm
-# from .utils import calculate_birthday_countdown
-
-
-# def birthday(request):
-#     form = BirthdayForm(request.GET or None)
-#     context = {'form': form}
-#     if form.is_valid():
-#         birthday_countdown = calculate_birthday_countdown(
-#             form.cleaned_data['birthday']
-#         )
-#     context.update({'birthday_countdown': birthday_countdown})
-#     return render(request, 'birthday/birthday.html', context)
