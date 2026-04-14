0f36ec9f0a52a3066244b78547b248590f0abe7e
Дарья Орлова <orlovadara70@gmail.com>
2026-04-14 18:21:55 +0500

    6 спринт 2 тема 2 урок до создания миксинов

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -6,6 +6,19 @@ from .utils import calculate_birthday_countdown
 from .models import Birthday
 # Импортируем класс пагинатора.
 from django.core.paginator import Paginator
+from django.views.generic import CreateView, ListView, UpdateView
+from django.urls import reverse_lazy
+
+
+class BirthdayCreateView(CreateView):
+    # Указываем модель, с которой работает CBV...
+    model = Birthday
+    form_class = BirthdayForm
+    # Явным образом указываем шаблон:
+    template_name = 'birthday/birthday.html'
+    # Указываем namespace:name страницы, куда будет перенаправлен пользователь
+    # после создания объекта:
+    success_url = reverse_lazy('birthday:list') 
 
 
 def birthday(request, pk=None):
@@ -30,21 +43,21 @@ def birthday(request, pk=None):
     return render(request, 'birthday/birthday.html', context) 
 
 
-def birthday_list(request):
-    # Получаем список всех объектов с сортировкой по id.
-    birthdays = Birthday.objects.order_by('id')
-    # Создаём объект пагинатора с количеством 10 записей на страницу.
-    paginator = Paginator(birthdays, 10)
-    # Получаем из запроса значение параметра page.
-    page_number = request.GET.get('page')
-    # Получаем запрошенную страницу пагинатора. 
-    # Если параметра page нет в запросе или его значение не приводится к числу,
-    # вернётся первая страница.
-    page_obj = paginator.get_page(page_number)
-    # Вместо полного списка объектов передаём в контекст 
-    # объект страницы пагинатора
-    context = {'page_obj': page_obj}
-    return render(request, 'birthday/birthday_list.html', context)
+class BirthdayUpdateView(UpdateView):
+    model = Birthday
+    form_class = BirthdayForm
+    template_name = 'birthday/birthday.html'
+    success_url = reverse_lazy('birthday:list')
+
+
+# Наследуем класс от встроенного ListView:
+class BirthdayListView(ListView):
+    # Указываем модель, с которой работает CBV...
+    model = Birthday
+    # ...сортировку, которая будет применена при выводе списка объектов:
+    ordering = 'id'
+    # ...и даже настройки пагинации:
+    paginate_by = 10 
 
 
 def delete_birthday(request, pk):
