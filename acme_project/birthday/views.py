9b98c3469be7029f8ab85d3e282f90e4dc1bfb54
Дарья Орлова <orlovadara70@gmail.com>
2026-04-14 19:29:57 +0500

    6 спринт 2 тема 3 урок

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,44 +1,44 @@
-from django.shortcuts import render, get_object_or_404, redirect
-from .forms import BirthdayForm
-# Импортируем из utils.py функцию для подсчёта дней.
-from .utils import calculate_birthday_countdown
-from .models import Birthday
-# Импортируем класс пагинатора.
-from django.core.paginator import Paginator
-from django.views.generic import CreateView, DeleteView, ListView, UpdateView
+from django.views.generic import (
+    CreateView, DeleteView, DetailView, ListView, UpdateView
+)
 from django.urls import reverse_lazy
 
-# Создаём миксин.
-class BirthdayMixin:
+from .forms import BirthdayForm
+from .models import Birthday
+from .utils import calculate_birthday_countdown
+
+
+class BirthdayListView(ListView):
+    model = Birthday
+    ordering = 'id'
+    paginate_by = 10
+
+
+class BirthdayCreateView(CreateView):
+    model = Birthday
+    form_class = BirthdayForm
+
+
+class BirthdayUpdateView(UpdateView):
+    model = Birthday
+    form_class = BirthdayForm
+
+
+class BirthdayDeleteView(DeleteView):
     model = Birthday
     success_url = reverse_lazy('birthday:list')
 
-class BirthdayFormMixin:
-    form_class = BirthdayForm
-    template_name = 'birthday/birthday.html'
 
-
-# Добавляем миксин первым по списку родительских классов.
-class BirthdayCreateView(BirthdayMixin, BirthdayFormMixin, CreateView):
-    # Не нужно описывать атрибуты: все они унаследованы от BirthdayMixin.
-    pass
-
-
-class BirthdayUpdateView(BirthdayMixin, BirthdayFormMixin, UpdateView):
-    # И здесь все атрибуты наследуются от BirthdayMixin.
-    pass 
-
-
-# Наследуем класс от встроенного ListView:
-class BirthdayListView(ListView):
-    # Указываем модель, с которой работает CBV...
+class BirthdayDetailView(DetailView):
     model = Birthday
-    # ...сортировку, которая будет применена при выводе списка объектов:
-    ordering = 'id'
-    # ...и даже настройки пагинации:
-    paginate_by = 10 
 
-
-class BirthdayDeleteView(BirthdayMixin, DeleteView):
-    model = Birthday
-    success_url = reverse_lazy('birthday:list') 
+    def get_context_data(self, **kwargs):
+        # Получаем словарь контекста:
+        context = super().get_context_data(**kwargs)
+        # Добавляем в словарь новый ключ:
+        context['birthday_countdown'] = calculate_birthday_countdown(
+            # Дату рождения берём из объекта в словаре context:
+            self.object.birthday
+        )
+        # Возвращаем словарь контекста.
+        return context
